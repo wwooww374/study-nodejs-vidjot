9b04f8f28f2da1247e6979c1b001619224484f6b
Jieun Kim <wwooww374@gmail.com>
2018-08-19 11:58:07 +0900

    add routes

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');     // bring the package 'express'
 
 const app = express();
 
+// routes
+app.get('/', (req, res) => {
+    res.send('INDEX');
+});
+
+app.get('/about', (req, res) => {
+    res.send('ABOUT');
+});
+
+
 const port = 5000;
 
 app.listen(port, () => {    // arrow function (ES6)
     console.log(`Server started on port ${port}...`);   // string embedding (ES6)
     // console.log('Server started on ' + port + '...');
-})
+});
