ac6cd5e593ed83bf87b5d61ed56ee25b0f980a25
Jieun Kim <wwooww374@gmail.com>
2018-08-21 13:09:59 +0900

    edit idea

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const app = express();
 
@@ -27,6 +28,9 @@ app.set('view engine', 'handlebars');
 // body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// method-override middleware
+app.use(methodOverride('_method'));
 
 // routes
 app.get('/', (req, res) => {
@@ -54,6 +58,16 @@ app.get('/ideas/add', (req, res) => {
     res.render('ideas/add')
 });
 
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    }).then(idea => {
+        res.render('ideas/edit', {
+            idea: idea
+        });
+    });
+});
+
 app.post('/ideas', (req, res) => {
     let errors = [];
 
@@ -77,6 +91,19 @@ app.post('/ideas', (req, res) => {
                 res.redirect('/ideas');
             });
     }
+});
+
+app.put('/ideas/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    }).then(idea => {
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+        idea.save()
+            .then(idea => {
+                res.redirect('/ideas');
+            });
+    });
 });
 
 
